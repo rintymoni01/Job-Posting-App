dffb0690b58032727f807656e418239ec1c54eba
rintymoni01 <rintymoni01@gmail.com>
2026-05-01 19:58:23 +0600

    Add custom_text_field.dart widget

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+
+  const CustomTextField({Key? key, required this.hintText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+}
