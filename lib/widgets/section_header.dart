e993e78225ab7f7cd3ea33653012e88d6811ee0d
rintymoni01 <rintymoni01@gmail.com>
2026-05-01 19:58:40 +0600

    Add section_header.dart widget

---
+++ lib/widgets/section_header.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+
+  const SectionHeader({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
